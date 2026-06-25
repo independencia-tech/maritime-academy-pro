a74686958ba71c14882ee21962699ecdffeea80b
Alpha237-wise <alphakolondo@gmail.com>
2026-06-25 16:19:20 +0300

    feat: LessonE3_L5 Sécurités & Alarmes chaudière

--- src/components/MaritimeApp.tsx
+++ src/components/MaritimeApp.tsx
@@ -29,6 +29,7 @@ import LessonE3_L1 from "./LessonE3_L1";
 import LessonE3_L2 from "./LessonE3_L2";
 import LessonE3_L3 from "./LessonE3_L3";
 import LessonE3_L4 from "./LessonE3_L4";
+import LessonE3_L5 from "./LessonE3_L5";
 import LessonIncendie from "./LessonIncendie";
 import LessonSauvetage from "./LessonSauvetage";
 import LessonMARPOL from "./LessonMARPOL";
@@ -2198,6 +2199,13 @@ else if (m?.id === "e7") setPage("e7_lessons");
     lang={lang}
     onBack={() => setPage("e3_lessons")}
     onComplete={() => { markLessonCompleted("e3-l4"); setPage("e3_lessons"); }}
+  />
+)}
+      {page === "lesson_e3_l5" && (
+  <LessonE3_L5
+    lang={lang}
+    onBack={() => setPage("e3_lessons")}
+    onComplete={() => { markLessonCompleted("e3-l5"); setPage("e3_lessons"); }}
   />
 )}
 {page === "e6_lessons" && (
